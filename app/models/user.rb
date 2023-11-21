5d866c5375b13759e5fd1ecd119dfee0676dedc9
Wesley-waka <wesleywaka77@gmail.com>
2023-11-21 12:18:51 +0300

    Activation edit action

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,9 +33,10 @@ class User < ApplicationRecord
     end
 
     # returns true if the given token matched the digest
-    def authenticated?(remember_token)
-        return false if (remember_digest.nil)
-        BCrypt::Password.new(:remmber_digest).is_password(remember_token)
+    def authenticated?(attribute, token)
+        digest = self.send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
     end
 
     private
