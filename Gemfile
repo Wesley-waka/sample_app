d24a7a7e3a56f80bbbbd709ea4be3a28a2f66bbb
Wesley-waka <wesleywaka77@gmail.com>
2023-11-21 06:04:08 +0300

    Finish user edit,update,index and destroy

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ gem "bootsnap",        "1.12.0", require: false
 gem 'bootstrap-sass'
 gem "bcrypt"
 gem 'redis'
+
+gem 'faker'
+
 group :development, :test do
   gem "sqlite3", "1.4.2"
   gem "debug",   "1.5.0", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update,:destroy]
+  before_action :correct_user, only: [:edit, :update]
+  befoe_action :admin_user, only: [:destroy]
   # def show
   #   @user = User.find(params[:id])
   # end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     user_cache_key = "user_#{params[:id]}"
     cached_user = Rails.cache.read(user_cache_key)
@@ -34,12 +42,51 @@ class UsersController < ApplicationController
     end
   end
   
-  
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      # handle a successful update
+      flash[:success] = "Profile update"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:succes] = "User deleted"
+    redirect_to users_url
+    
+  end
 
   private 
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  # Before filters
   
+  # confirms a logged-in user
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # confirms an admin user
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
 end
