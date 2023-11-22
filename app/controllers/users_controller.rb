08d0ef048fed2b874570e8500ec7ee6b94e556ff
Wesley-waka <wesleywaka77@gmail.com>
2023-11-22 18:39:13 +0300

    Following and Followers Pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update,:destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update,:destroy,:following,:follwers]
+
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy]
   # def show
@@ -73,6 +74,19 @@ class UsersController < ApplicationController
     
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.pagination(page: params[:page])
+    render 'show_follow'
+  end
+
+  def following
+    @titl = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   private 
 
   def user_params
